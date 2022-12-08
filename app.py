dc31675554addb4798e59c8350ca49cd1de47f04
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-12-08 13:31:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello, World!'
+
+#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
+os.system("git clone https://github.com/Dkmovie/DK-Files-Store-Bot ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
